b8d950d7a1876a5837a3beffc8b0faf1c4557dc7
infapolis <Valentin.ivlev@gmail.com>
2022-07-14 08:53:19 +0900

    +

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Custom Grid Filters</title>
+
+    <!-- ** CSS ** -->
+    <!-- base library -->
+    <link rel="stylesheet" type="text/css" href="/js/ext-3.1.1/resources/css/ext-all.css" />
+
+    <!-- overrides to base library -->
+    <link rel="stylesheet" type="text/css" href="/js/ux/gridfilters/css/GridFilters.css" />
+    <link rel="stylesheet" type="text/css" href="/js/ux/gridfilters/css/RangeMenu.css" />
+
+    <!-- page specific -->
+    <link rel="stylesheet" type="text/css" href="/js/ux/test.css" />
+
+    <style type="text/css">
+    </style>
+
+    <!-- ** Javascript ** -->
+    <!-- ExtJS library: base/adapter -->
+    <script type="text/javascript" src="/js/ext-3.1.1/adapter/ext/ext-base.js"></script>
+
+    <!-- ExtJS library: all widgets -->
+    <script type="text/javascript" src="/js/ext-3.1.1/ext-all.js"></script>
+
+    <!-- overrides to base library -->
+
+    <!-- extensions -->
+    <script type="text/javascript" src="/js/ux/gridfilters/menu/RangeMenu.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/menu/ListMenu.js"></script>
+    
+    <script type="text/javascript" src="/js/ux/gridfilters/GridFilters.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/Filter.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/StringFilter.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/DateFilter.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/ListFilter.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/NumericFilter.js"></script>
+    <script type="text/javascript" src="/js/ux/gridfilters/filter/BooleanFilter.js"></script>
+
+    <!-- page specific -->
+    <script type="text/javascript" src="/js/ux/test.js"></script>
+    <script type="text/javascript" src="/js/grid-filter-local.js"></script>
+
+</head>
+<body>
+    <div id="grid-example" style="margin: 10px;"></div>
+</body>
+</html>
